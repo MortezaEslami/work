415610005adf6b7567c9165bac4485293f21a030
Morteza Eslami <mortezae@energizeglobal.com>
2022-11-27 21:39:56 +0400

    Add gateway module

---
+++ gateway/src/test/java/com/zerodefects/apigateway/ApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerodefects.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
